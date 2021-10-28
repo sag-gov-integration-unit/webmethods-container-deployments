4f41eba49f548880b09b9e0cceec28b081ac95bc
Darryn Graham <darryn.graham@softwareaggov.com>
2021-10-28 08:52:39 -0400

    Added scripts to control gateway only and added support for volumes on the gateway

---
+++ azure_aci/apigateway-and-portal/delete_gateway.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This file will delete all of the gateway containers in the standlone webMethods API Platform configuration.
+#
+
+#import variables for your Azure resources
+source ../configs/docker.env$SAG_RELEASE
+
+#perform the stop actions
+echo Deleting API Gateway...
+az container delete -y -n webmethods-apigateway -g $RESOURCEGROUP
+
+echo Deleting API Configurator...
+az container delete -y -n webmethods-gw-config -g $RESOURCEGROUP
