e7dd8fa75d3921c8c7dc9b8ae6a7b5dfd6d556b9
Darryn Graham <darryn.graham@softwareaggov.com>
2021-11-04 08:44:05 -0400

    corrected leading slash in scripts, added prefix for deploys

--- azure_aci/apigateway-and-portal/docker_pull.sh
+++ azure_aci/apigateway-and-portal/docker_pull.sh
@@ -5,28 +5,10 @@
 #
 
 #import variables for your Azure resources
-#source ../configs/docker.env$SAG_RELEASE
-REG=ghcr.io/softwareag-government-solutions/
-TAG_APIGATEWAY=dev-10.7-latest
-TAG_APIGATEWAY_CONFIGURATOR=configurator-10.7-latest
-TAG_ELASTIC_VERSION=7.7.1
-TAG_TERRACOTTA=dev-4.3.9-latest
-TAG_SAMPLE_APIS=dev-0.0.4
-TAG_APIPORTAL=dev-10.7.1-36
-SAG_PASSWORD=somethingnew
+source ../configs/docker.env$SAG_RELEASE
 
-#Set your Azure ACR Environment Specific Variables
-RESOURCEGROUP="DarrynsResources"
-REGISTRYLOGINSERVER="saggscloudimages.azurecr.us"
-REGISTRYUSERNAME="saggscloudimages"
-REGISTRYPASSWORD="fq3xMbfyNKOi8l1CFr/HRVvbsGTDFgfR"
-VOLUMEACCOUNTKEY=HgnC8OXBDyqb94I/C2/de627a05cbTlyW2iW3FumyyUsCuFagkDsohdUN3YPVNre5M8tOiOAKNXhJ7i4tYZccw==
-VOLUMEACCOUNTNAME=apistorage
-
-echo $REG
-
-docker pull $REG/webmethods-sample-apis-bookstore:$TAG_SAMPLE_APIS
-docker pull $REG/webmethods-sample-apis-uszip:$TAG_SAMPLE_APIS
-docker pull $REG/webmethods-apigateway-standalone:$TAG_APIGATEWAY
-docker pull $REG/webmethods-apigateway-configurator:$TAG_APIGATEWAY_CONFIGURATOR
-docker pull $REG/webmethods-apiportal:$TAG_APIPORTAL
+docker pull $REGwebmethods-sample-apis-bookstore:$TAG_SAMPLE_APIS
+docker pull $REGwebmethods-sample-apis-uszip:$TAG_SAMPLE_APIS
+docker pull $REGwebmethods-apigateway-standalone:$TAG_APIGATEWAY
+docker pull $REGwebmethods-apigateway-configurator:$TAG_APIGATEWAY_CONFIGURATOR
+docker pull $REGwebmethods-apiportal:$TAG_APIPORTAL
