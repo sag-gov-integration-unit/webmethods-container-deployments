c6a7a4517a4689bf8f85daf2c80e78310b715778
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2022-02-24 12:49:04 -0500

    updating deployment vcalues

--- kubernetes/api_management/apistack-clustered/deploy.sh
+++ kubernetes/api_management/apistack-clustered/deploy.sh
@@ -10,5 +10,11 @@ fi
 
 kubectl --namespace $NAMESPACE apply -f elasticsearch.yaml
 kubectl --namespace $NAMESPACE apply -f kibana.yaml
-helm upgrade -i --namespace $NAMESPACE -f devportal.yaml webmethods-devportal saggov-helm-charts/webmethods-devportal
-helm upgrade -i --namespace $NAMESPACE -f apigateway.yaml webmethods-apigateway saggov-helm-charts/webmethods-apigateway
+
+## from GA helm charts
+# helm upgrade -i --namespace $NAMESPACE -f devportal.yaml webmethods-devportal saggov-helm-charts/webmethods-devportal
+# helm upgrade -i --namespace $NAMESPACE -f apigateway.yaml webmethods-apigateway saggov-helm-charts/webmethods-apigateway
+
+## from local helm charts (ie. for development)
+helm upgrade -i --namespace $NAMESPACE -f devportal.yaml webmethods-devportal ${SAGGOV_HELMCHART_HOME}/webmethods-devportal
+helm upgrade -i --namespace $NAMESPACE -f apigateway.yaml webmethods-apigateway ${SAGGOV_HELMCHART_HOME}/webmethods-apigateway
--- kubernetes/api_management/apistack-clustered/sampleapis_microgateway/deploy.sh
+++ kubernetes/api_management/apistack-clustered/sampleapis_microgateway/deploy.sh
@@ -8,8 +8,10 @@ if [ "x$NAMESPACE" == "x" ]; then
     exit 2;
 fi
 
-# helm upgrade -i --namespace $NAMESPACE -f sampleapis-bookstore.yaml sampleapis-bookstore saggov-helm-charts/sample-java-apis
-# helm upgrade -i --namespace $NAMESPACE -f sampleapis-uszip.yaml sampleapis-uszip saggov-helm-charts/sample-java-apis
+## from GA helm charts
+# helm upgrade -i --namespace $NAMESPACE -f sampleapis-bookstore.yaml sampleapis-mgw-bookstore saggov-helm-charts/samplejavaapis-sidecar-microgateway
+# helm upgrade -i --namespace $NAMESPACE -f sampleapis-uszip.yaml sampleapis-mgw-uszip saggov-helm-charts/samplejavaapis-sidecar-microgateway
 
-helm upgrade -i --namespace $NAMESPACE -f sampleapis-mgw-bookstore.yaml sampleapis-mgw-bookstore ${HOME}/MyDev/github_saggs/softwareag_government_solutions/saggov-helm-charts/src/sample-java-apis
-helm upgrade -i --namespace $NAMESPACE -f sampleapis-mgw-uszip.yaml sampleapis-mgw-uszip ${HOME}/MyDev/github_saggs/softwareag_government_solutions/saggov-helm-charts/src/sample-java-apis
+## from local helm charts (ie. for development)
+helm upgrade -i --namespace $NAMESPACE -f sampleapis-mgw-bookstore.yaml sampleapis-mgw-bookstore ${SAGGOV_HELMCHART_HOME}/samplejavaapis-sidecar-microgateway
+helm upgrade -i --namespace $NAMESPACE -f sampleapis-mgw-uszip.yaml sampleapis-mgw-uszip ${SAGGOV_HELMCHART_HOME}/samplejavaapis-sidecar-microgateway
