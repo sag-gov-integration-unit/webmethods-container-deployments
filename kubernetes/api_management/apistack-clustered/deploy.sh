f06714efcd68e79eda546b33de3831ec348fc410
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2022-02-28 16:14:56 -0500

    using helm chart repo

--- kubernetes/api_management/apistack-clustered/deploy.sh
+++ kubernetes/api_management/apistack-clustered/deploy.sh
@@ -11,10 +11,10 @@ fi
 kubectl --namespace $NAMESPACE apply -f elasticsearch.yaml
 kubectl --namespace $NAMESPACE apply -f kibana.yaml
 
-## from GA helm charts
-# helm upgrade -i --namespace $NAMESPACE -f devportal.yaml webmethods-devportal saggov-helm-charts/webmethods-devportal
-# helm upgrade -i --namespace $NAMESPACE -f apigateway.yaml webmethods-apigateway saggov-helm-charts/webmethods-apigateway
+## from helm chart public repo
+helm upgrade -i --namespace $NAMESPACE -f devportal.yaml webmethods-devportal saggov-helm-charts/webmethods-devportal
+helm upgrade -i --namespace $NAMESPACE -f apigateway.yaml webmethods-apigateway saggov-helm-charts/webmethods-apigateway
 
 ## from local helm charts (ie. for development)
-helm upgrade -i --namespace $NAMESPACE -f devportal.yaml webmethods-devportal ${SAGGOV_HELMCHART_HOME}/webmethods-devportal
-helm upgrade -i --namespace $NAMESPACE -f apigateway.yaml webmethods-apigateway ${SAGGOV_HELMCHART_HOME}/webmethods-apigateway
+# helm upgrade -i --namespace $NAMESPACE -f devportal.yaml webmethods-devportal ${SAGGOV_HELMCHART_HOME}/webmethods-devportal
+# helm upgrade -i --namespace $NAMESPACE -f apigateway.yaml webmethods-apigateway ${SAGGOV_HELMCHART_HOME}/webmethods-apigateway
