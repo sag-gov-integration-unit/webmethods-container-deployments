34cc263516cdaa1cc535cdbf25db181621ea8997
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2022-02-28 18:59:55 -0500

    updating

--- kubernetes/api_management/apistack-clustered/sampleapis_microgateway/deploy.sh
+++ kubernetes/api_management/apistack-clustered/sampleapis_microgateway/deploy.sh
@@ -9,8 +9,8 @@ if [ "x$NAMESPACE" == "x" ]; then
 fi
 
 ## from GA helm charts
-helm upgrade -i --namespace $NAMESPACE -f sampleapis-bookstore.yaml sampleapis-mgw-bookstore saggov-helm-charts/samplejavaapis-sidecar-microgateway
-helm upgrade -i --namespace $NAMESPACE -f sampleapis-uszip.yaml sampleapis-mgw-uszip saggov-helm-charts/samplejavaapis-sidecar-microgateway
+helm upgrade -i --namespace $NAMESPACE -f sampleapis-mgw-bookstore.yaml sampleapis-mgw-bookstore saggov-helm-charts/samplejavaapis-sidecar-microgateway
+helm upgrade -i --namespace $NAMESPACE -f sampleapis-mgw-uszip.yaml sampleapis-mgw-uszip saggov-helm-charts/samplejavaapis-sidecar-microgateway
 
 ## from local helm charts (ie. for development)
 # helm upgrade -i --namespace $NAMESPACE -f sampleapis-mgw-bookstore.yaml sampleapis-mgw-bookstore ${SAGGOV_HELMCHART_HOME}/samplejavaapis-sidecar-microgateway
